828006c058753ec4f2880b979e897147159b4deb
naoto24kawa <naoto24kawa@gmail.com>
2025-07-01 09:39:15 +0900

    全プラットフォームの正しいSHA256ハッシュを追加

--- mcpjson.rb
+++ mcpjson.rb
@@ -7,22 +7,22 @@ class Mcpjson < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/naoto24kawa/mcpjson/releases/download/v0.2.3/mcpjson_Darwin_x86_64.tar.gz"
-      # sha256 will be calculated automatically
+      sha256 "8d68ea681f2d63f778bae6d33cec5beb78a8d638041ff5e420a2fbd42984c7e5"
     end
     if Hardware::CPU.arm?
       url "https://github.com/naoto24kawa/mcpjson/releases/download/v0.2.3/mcpjson_Darwin_arm64.tar.gz"
-      # sha256 will be calculated automatically
+      sha256 "c3436ee656224bd4590648600dc786127536d708db4dcca089a3552be760c811"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/naoto24kawa/mcpjson/releases/download/v0.2.3/mcpjson_Linux_x86_64.tar.gz"
-      # sha256 will be calculated automatically
+      sha256 "dc8c368bcd7a8b4a5d21a878bafd6bb87cfabae32721bd3159a03591f2ed58b9"
     end
     if Hardware::CPU.arm?
       url "https://github.com/naoto24kawa/mcpjson/releases/download/v0.2.3/mcpjson_Linux_arm64.tar.gz"
-      # sha256 will be calculated automatically
+      sha256 "3bba78e96d13169c3005bf18adf69c8efacd090396332af3c3cb4861be79e7d7"
     end
   end
 
